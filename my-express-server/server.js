5814e1ebf6f9b3270a6f06e8f3fa122f8baa89a4
bocahrokok <indieunistudy@gmail.com>
2019-08-01 20:15:00 +0700

    commit serverjs

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req,res){
-    res.send("<h1>Hello, world!"</h1>);
+    Response.send("<h1>Hello, world!</h1>");
 
 });
 
