7af2400c765669fa5f1d20ae3922a8b458331979
bocahrokok <indieunistudy@gmail.com>
2019-08-02 18:14:39 +0700

    commit

--- bitcoin-ticker/index.js
+++ bitcoin-ticker/index.js
@@ -9,5 +9,5 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.listen(3000, function(req, res){
-    console.log("asu bajingan")
+    console.log("asu bajingan 3000")
 })
