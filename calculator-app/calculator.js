5419fb912ce1ef9e5eb812e846e4fdead36fdb65
bocahrokok <indieunistudy@gmail.com>
2019-08-02 11:03:27 +0700

    commit

--- calculator-app/calculator.js
+++ calculator-app/calculator.js
@@ -1,11 +1,21 @@
+//jshint esversion:6
+
 
 const express = require("express");
 
 const app = express();
 
-app.get("/", function(res,req){
-    res.setEncoding("Iki rumah su");
-})
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/index.html")
+    //res.sendFile("index.html");
+});
+
+
+app.post("/", function(req, res){
+    res.send("Thanks for sending that");
+});
+
+
 
 app.listen(4000, function(){
     console.log("Server started on port 4000");
