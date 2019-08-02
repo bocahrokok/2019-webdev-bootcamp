805be827d0b8c39aed38193ad23c3aecef1a53c5
bocahrokok <indieunistudy@gmail.com>
2019-08-02 11:15:09 +0700

    final calculator

--- calculator-app/calculator.js
+++ calculator-app/calculator.js
@@ -18,8 +18,12 @@ app.get("/", function(req,res){
 
 app.post("/", function(req, res){
 
-    console.log(req.body);
-    res.send("Thanks for sending that");
+    var num1 = req.body.num1;
+    var num2 = req.body.num2;
+
+    var result = num1 + num2; 
+
+    res.send("The result of the calculation is" + result);
 });
 
 
