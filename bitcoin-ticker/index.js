6bb7ace9db92fdf376e5d369e2c26fe902600517
bocahrokok <indieunistudy@gmail.com>
2019-08-03 07:01:16 +0700

    index commit

--- bitcoin-ticker/index.js
+++ bitcoin-ticker/index.js
@@ -9,23 +9,31 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req,res){
-    res.sendFile(__dirname + "/index.html")
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function(req, res){
 
+    var crypto = req.body.crypto;
+    var fiat = req.body.fiat;
 
+    var baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/"
+    
+    var finalURL = baseURL + crypto + fiat;
 
-    request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", function(error, response, body){
-        
-    })
+    request("finalURL", function(error, response, body){
+
+        var data = JSON.parse(body);
+        var price = data.averages.week;
+
+        res.send("<h1> The current price of "+ crypto + " is " + price +  "USD </h1>");
+
+    });
     
     
-})
-
-
+});
 
 
 app.listen(3000, function(req, res){
-    console.log("asu bajingan 3000")
-})
+    console.log("asu bajingan 3000");
+});
