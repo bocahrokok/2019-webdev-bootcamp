186ae02b5e59bd707b6003216727625ee8d9d427
bocahrokok <indieunistudy@gmail.com>
2019-08-04 21:13:52 +0700

    commit appjs

--- newsletter-signup/app.js
+++ newsletter-signup/app.js
@@ -1,3 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+
+const app = express();
+
+app.listen(3000, function(req, res){
+    console.log("port listen to i love you port 3000")
+})
