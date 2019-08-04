b21fb6d899fa7ab9843d46928f6c2f9df0b7a5f9
bocahrokok <indieunistudy@gmail.com>
2019-08-04 23:16:22 +0700

    singup

--- newsletter-signup/app.js
+++ newsletter-signup/app.js
@@ -7,6 +7,7 @@ const app = express();
 
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.get("/", function(req,res){
@@ -14,6 +15,22 @@ app.get("/", function(req,res){
 });
 
 
+app.post("/", function(req, res){
+    
+    var firstName =  res.body.fName;
+    var lastName = res.body.lName;
+    var email = res.body.email;
+
+    console.log(firstName, lastName, email)
+});
+
+
+
+
+
+
+
 app.listen(3000, function(req, res){
     console.log("port listen to i love you port 3000")
 });
+
